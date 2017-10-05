86fa0805b032303ba4e9101ae16fa80059794c79
syhsyh9696 <sunyuanhao123456@gmail.com>
2017-10-05 14:56:38 +0000

    Add search video method

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,9 @@
 class ActorsController < ApplicationController
   def index
     @actors = Actor.paginate(:page => params[:page], :per_page => 30)
+
+    @q = Actor.ransack(params[:q])
+    @actor = @q.result(distinct: true)
   end
 
   def show
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  before_action :validate_search_key, only: [:search]
+  before_action :validate_search_key, only: [:index, :search]
   def index
     @videos = Video.paginate(:page => params[:page], :per_page => 30)
   end
@@ -9,11 +9,17 @@ class VideosController < ApplicationController
   end
 
   def search
-
+    if @query_string.present?
+      @videos = search_params
+    end
   end
 
 protected
   def validate_search_key
     @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+  end
+
+  def search_params
+    Video.ransack({:video_id_cont => @query_string}).result(distinct: true)
   end
 end
