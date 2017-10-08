6c37d10225b71abbb7709d59ef1023c746dee945
syhsyh9696 <sunyuanhao123456@gmail.com>
2017-10-08 07:51:46 +0000

    Add favicon.ico in public

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :videos do
+  resources :videos, only:[:index, :show] do
     collection do
       get :search
     end
   end
 
-  resources :actors
-  resources :stars
+  resources :actors, only:[:index, :show]
+  resources :stars, only:[:index, :show]
   root 'welcome#index'
 end
