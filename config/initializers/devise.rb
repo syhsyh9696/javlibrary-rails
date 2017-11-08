c4acd08a054834fd2a65047546bfc67dccb534a1
syhsyh9696 <sunyuanhao123456@gmail.com>
2017-11-08 08:00:08 +0000

    Update devise session new.html.erb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -35,7 +35,8 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
-  config.authentication_keys = [:username, :email]
+  # config.authentication_keys = [:username, :email]
+
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
